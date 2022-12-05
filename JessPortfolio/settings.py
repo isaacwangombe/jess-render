a601f6fde1056db6e24ad697d4a7baa77a3aedac
Isaac Wangombe <isaacwangombe5@gmail.com>
2022-12-05 16:29:45 +0300

    deployment

--- JessPortfolio/settings.py
+++ JessPortfolio/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
